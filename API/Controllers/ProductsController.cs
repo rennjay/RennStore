415d3954bdcf3f4ac4c928faca823a67e4b9c3d7
Rennjay Soterio <rennjay.soterio@betterask.erni>
2023-11-01 22:29:34 +0800

    Seed data

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly StoreContext context;
+    private readonly ILogger logger;
+    public ProductsController(StoreContext context, ILogger logger)
+    {
+        this.logger = logger;
+        this.context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<List<Product>> GetProducts()
+    {
+        var products = context.Products.ToList();
+        return Ok(products);
+    }
+}
