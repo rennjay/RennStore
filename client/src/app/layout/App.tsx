8cf96c33ae92ab90b0ba70d50d2e5a5df12c7884
Rennjay Soterio <rennjay.soterio@betterask.erni>
2023-11-09 22:17:49 +0800

    Implement darkmode switch

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,14 +1,27 @@
 import Catalog from "../../features/catalog/Catalog";
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, createTheme } from "@mui/material";
 import Header from "./Header";
+import { useState } from "react";
+import { ThemeProvider } from "@emotion/react";
 
 function App() {
+  const [isDarkMode, setDarkMode] = useState(false);
+  const paletteType = isDarkMode ? "dark" : "light";
+  const themeContext = createTheme({
+    palette: {
+      mode: paletteType,
+    },
+  });
+
+  function handleThemeChange() {
+    setDarkMode(!isDarkMode);
+  }
   return (
-    <>
+    <ThemeProvider theme={themeContext}>
       <CssBaseline />
-      <Header />
+      <Header handleThemeChange={handleThemeChange} isDarkMode={isDarkMode} />
       <Catalog />
-    </>
+    </ThemeProvider>
   );
 }
 
--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,10 +1,19 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
 
-export default function Header() {
+interface Props {
+  isDarkMode: boolean;
+  handleThemeChange: (isDarkMode: boolean) => void;
+}
+
+export default function Header({ handleThemeChange, isDarkMode }: Props) {
   return (
     <AppBar position="static" sx={{ mb: 3 }}>
       <Toolbar>
         <Typography variant="h6">Renn-Store</Typography>
+        <Switch
+          checked={isDarkMode}
+          onChange={() => handleThemeChange(!isDarkMode)}
+        />
       </Toolbar>
     </AppBar>
   );
