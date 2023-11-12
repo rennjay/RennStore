4ad71bade693960a3a373e62d19a3c1d973bfa4a
Rennjay Soterio <rennjay.soterio@betterask.erni>
2023-11-12 10:42:00 +0800

    Add axios and product call

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -9,6 +9,7 @@ import {
   Typography,
 } from "@mui/material";
 import Product from "../../app/models/Product";
+import { Link } from "react-router-dom";
 
 interface Props {
   product: Product;
@@ -52,7 +53,9 @@ export default function ProductCard({ product }: Props) {
         </CardContent>
         <CardActions>
           <Button size="small">ADD TO CART</Button>
-          <Button size="small">VIEW</Button>
+          <Button size="small" component={Link} to={"/catalog/" + product.id}>
+            VIEW
+          </Button>
         </CardActions>
       </Card>
     </>
--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,5 +1,28 @@
 import { Typography } from "@mui/material";
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import Product from "../../app/models/Product";
+//TODO: geturlid from search bar
+// load product
+// call from axios
+// loading state
 export default function ProductDetailsPage() {
-  return <Typography variant="h2">ProductDetails Page</Typography>;
+  const { id } = useParams<{ id: string }>();
+  const [product, setProduct] = useState<Product | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/products/${id}`)
+      .then((response) => setProduct(response.data))
+      .catch((error) => console.log(error))
+      .finally(() => setIsLoading(false));
+  }, [id]);
+
+  if (isLoading) return <h4>Loading...</h4>;
+
+  if (!product) return <h4>No Product Found!</h4>;
+
+  return <Typography variant="h2">{product.name}</Typography>;
 }
