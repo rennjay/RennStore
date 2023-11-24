12bb4cab68d26647cbdb7664bff41af23db1d5be
Rennjay Soterio <rennjay.soterio@betterask.erni>
2023-11-24 13:23:37 +0800

    add agent

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -2,14 +2,13 @@ import { Button, Container } from "@mui/material";
 import Product from "../../app/models/Product";
 import ProductList from "./ProductList";
 import { useState, useEffect } from "react";
+import agent from "../../app/api/agent";
 
 export default function Catalog() {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/products")
-      .then((response) => response.json())
-      .then((data) => setProducts(data));
+    agent.Catalog.list().then((data) => setProducts(data));
   }, []);
 
   return (
