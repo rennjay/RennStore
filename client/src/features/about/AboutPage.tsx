ddb69fae4c62a78178b8fbaaceafb5c458f9a817
Rennjay <rennjaysoterio@gmail.com>
2023-11-24 06:23:47 -0800

    Add testing errors buttons

--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -1,5 +1,33 @@
-import { Typography } from "@mui/material";
+import { Button, ButtonGroup, Container, Typography } from "@mui/material";
+import agent from "../../app/api/agent";
 
 export default function AboutPage() {
-  return <Typography variant="h2">About Page</Typography>;
+  return (
+    <Container>
+      <Typography variant="h2" gutterBottom>
+        Errors for testing purposes
+      </Typography>
+      <ButtonGroup
+        variant="contained"
+        aria-label="outlined primary button group"
+        fullWidth
+      >
+        <Button onClick={() => agent.TestErrors.get400Error()}>
+          Get 400 Error
+        </Button>
+        <Button onClick={() => agent.TestErrors.get401Error()}>
+          Get 401 Error
+        </Button>
+        <Button onClick={() => agent.TestErrors.get404Error()}>
+          Get 404 Error
+        </Button>
+        <Button onClick={() => agent.TestErrors.get500Error()}>
+          Get 500 Error
+        </Button>
+        <Button onClick={() => agent.TestErrors.getvalidationError()}>
+          Get Validation Error
+        </Button>
+      </ButtonGroup>
+    </Container>
+  );
 }
