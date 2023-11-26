8651435fd4bf500e1cfb6aa26a25905f08a0c683
Rennjay <rennjaysoterio@gmail.com>
2023-11-26 22:08:26 +0800

    Create Basket Entities

---
+++ API/Entities/Basket.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace API.Entities
+{
+    public class Basket
+    {
+        public int Id { get; set; }
+        public string BuyerId { get; set; }
+        public List<BasketItem> Items { get; set; } = new();
+
+        public void AddItem(Product product, int quantity)
+        {
+            if (Items.All(item => item.ProductId != product.Id))
+            {
+                Items.Add(new BasketItem { Product = product, Quantity = quantity });
+                return;
+            }
+
+            var existingItem = Items.FirstOrDefault(item => item.Id == product.Id);
+
+            if (existingItem is not null)
+            {
+                existingItem.Quantity += quantity;
+            }
+        }
+
+        public void RemoveItem(int productId, int quantity)
+        {
+            var item = Items.FirstOrDefault(item => item.ProductId == productId);
+
+            if (item == null)
+            {
+                return;
+            }
+
+            item.Quantity -= quantity;
+
+            if (item.Quantity == 0)
+            {
+                Items.Remove(item);
+            }
+        }
+    }
+}
