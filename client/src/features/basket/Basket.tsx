e6090b4579c4997832db3a9b7e5bdcbb823fcc26
Rennjay Soterio <rennjaysoterio@gmail.com>
2024-01-04 11:59:54 +0800

    Refactor Basket component to display items in a table

--- client/src/features/basket/Basket.tsx
+++ client/src/features/basket/Basket.tsx
@@ -1,8 +1,19 @@
-import { Typography } from "@mui/material";
+import {
+  IconButton,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
 import LoadingComponent from "../../app/layout/LoadingComponent";
 import { useEffect, useState } from "react";
 import agent from "../../app/api/agent";
 import { Basket } from "../../app/models/Basket";
+import { Delete } from "@mui/icons-material";
 
 export default function Basket() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
@@ -19,5 +30,43 @@ export default function Basket() {
 
   if (!basket) return <Typography variant="h3"></Typography>;
 
-  return <Typography variant="h3">BuyerId: {basket.buyerId}</Typography>;
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Product</TableCell>
+            <TableCell align="right">Price</TableCell>
+            <TableCell align="right">Quantity</TableCell>
+            <TableCell align="right">Subtotal</TableCell>
+            <TableCell align="right"></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {basket.items.map((item) => (
+            <TableRow
+              key={item.productId}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {item.name}
+              </TableCell>
+              <TableCell align="right">
+                {(item.price / 100).toFixed(2)}
+              </TableCell>
+              <TableCell align="right">{item.quantity}</TableCell>
+              <TableCell align="right">
+                {((item.price / 100) * item.quantity).toFixed(2)}
+              </TableCell>
+              <TableCell align="right">
+                <IconButton color="error">
+                  <Delete />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
